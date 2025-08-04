411296ffefdad3625759ce37c22913757678c558
Tangerinn <tanayapawar248@gmail.com>
2025-08-04 19:56:35 +0530

    Change 2 has been made, which shall be recorded as a version

--- Demo.java
+++ Demo.java
@@ -3,5 +3,12 @@ class Demo
     public static void main(String[] args) 
     {
         System.out.println("Hello github!!!"); 
+        System.out.println("This is change 1"); 
+        // staged here; meaning, saved code uptill here. It means that I could commit; I could record this as a version if I wanted to, using the git commit command.
+        // every change may or may not be committed (saved as a version), but you can still save your changes, by staging.
+        // now, I will write a print "This is change 2", and push it on github. (Stage, then commit, then push.)
+
+        System.out.println("This is change 2");
+
     }
 }
