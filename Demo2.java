64337a726b2db147d0636350bb94e0a73774525e
Tangerinn <tanayapawar248@gmail.com>
2025-08-06 10:24:06 +0530

    change 3 made.

--- Demo2.java
+++ Demo2.java
@@ -6,3 +6,12 @@ class Demo2
         System.out.println("Change 2 made.");
     }
 }
+
+class Demo3
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Created Demo2.java for studying the diff command.");
+        System.out.println("Change 2 made.");
+    }
+}
